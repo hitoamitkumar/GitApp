e3ea102fd95c781ef2a7ccead8c1dfd842c7cc42
amitkumar <v-akumar12@expedia.com>
2017-01-02 02:18:53 +0530

    local 2 from local

--- A.java
+++ A.java
@@ -4,6 +4,7 @@ public class A {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World From Git online 1");
         System.out.println("Hello, World From Local 1");
+        System.out.println("Hello, World From Local 2");
     }
 
 }
