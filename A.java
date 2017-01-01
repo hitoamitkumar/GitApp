da66b0729de66bdeb33aad76ee23643f038de38e
amitkumar <v-akumar12@expedia.com>
2017-01-02 02:10:22 +0530

    from local added B.java and A edit

--- A.java
+++ A.java
@@ -3,6 +3,7 @@ public class A {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World From Git online 1");
+        System.out.println("Hello, World From Local 1");
     }
 
 }
